45a5bd241f52b361ca3391a6f11905fc164e97a6
Gabor Ratky <gabor@secretsaucepartners.com>
2016-09-16 00:23:14 +0200

    Implement the world's simplest Spark REPL

--- awsh/__init__.py
+++ awsh/__init__.py
@@ -1,12 +1,34 @@
 from __future__ import unicode_literals, print_function
 
+import atexit
+
 from prompt_toolkit import prompt
+from pyspark.sql import SparkSession
+
+
+def getOrCreateSpark():
+    return SparkSession.builder.getOrCreate()
 
 
 def main():
+    print("Initializing Spark")
+    spark = getOrCreateSpark()
+    sc = spark.sparkContext
+    atexit.register(lambda: sc.stop())
+
+    print("""
+Welcome to                     __
+          ____ __      _______/ /_
+         / __ `/ | /| / / ___/ __ \\
+        / /_/ /| |/ |/ (__  ) / / /
+        \__,_/ |__/|__/____/_/ /_/
+""")
+
     while True:
         try:
-            prompt('>>> ')
+            text = prompt('>>> ')
+            code = compile(text, 'awsh', 'single')
+            exec(code)
         except (KeyboardInterrupt, EOFError):
             break
 
