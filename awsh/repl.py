e9cd83329316a5f6011cc9b8d3ae573918b3fa35
Gabor Ratky <gabor@secretsaucepartners.com>
2016-09-18 16:32:09 +0200

    Encapsulate repl into awsh.repl and ReplSession

--- awsh/repl.py
+++ awsh/repl.py
@@ -1,26 +1,53 @@
-import atexit
+from __future__ import unicode_literals, print_function
+
 import traceback
 from codeop import compile_command
 from subprocess import call
 
+import atexit
 import sys
 from prompt_toolkit import prompt
 from prompt_toolkit.history import InMemoryHistory
 from pyspark.sql import SparkSession
 
 
-def getOrCreateSpark():
-    return SparkSession.builder \
-                       .appName("awsh") \
-                       .enableHiveSupport() \
-                       .getOrCreate()
+class ReplSession(object):
+    def __init__(self):
+        self.history = InMemoryHistory()
+        self.spark = self.get_or_create_spark_context()
+        self.sc = self.spark.sparkContext
+        atexit.register(lambda: self.sc.stop())
+
+    def prompt(self):
+        text = prompt('>>> ', history=self.history)
+        if text:
+            self.handle_input(text)
+
+    def handle_input(self, text):
+        # determine what to do with input text
+        if text[0] == '!':
+            self.exec_shell(text[1:])
+        else:
+            self.exec_code(text)
+
+    @staticmethod
+    def exec_code(text):
+        exec(compile_command(text))
+
+    @staticmethod
+    def exec_shell(text):
+        call(text, shell=True)
+
+    @staticmethod
+    def get_or_create_spark_context():
+        return SparkSession.builder \
+            .appName("awsh") \
+            .enableHiveSupport() \
+            .getOrCreate()
 
 
 def run():
-    print("Initializing Spark")
-    spark = getOrCreateSpark()
-    sc = spark.sparkContext
-    atexit.register(lambda: sc.stop())
+    session = ReplSession()
 
     print("""
 Welcome to                     __
@@ -30,31 +57,11 @@ Welcome to                     __
         \__,_/ |__/|__/____/_/ /_/
 """)
 
-    history = InMemoryHistory()
-
     while True:
         try:
-            text = prompt('>>> ', history=history)
-            if text:
-                handle_input(text)
+            session.prompt()
         except (KeyboardInterrupt, EOFError):
             break
         except Exception:
             traceback.print_exc(file=sys.stdout)
             continue
-
-
-def exec_code(text):
-    exec(compile_command(text))
-
-
-def exec_shell(text):
-    call(text, shell=True)
-
-
-def handle_input(text):
-    # determine what to do with input text
-    if text[0] == '!':
-        exec_shell(text[1:])
-    else:
-        exec_code(text)
--- awsh/session.py
+++ awsh/session.py
@@ -1,5 +1,3 @@
 from __future__ import unicode_literals, print_function
 
-from awsh.repl import ReplSession
 
-__all__ = [ReplSession]
